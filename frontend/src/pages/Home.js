9da50ff7fdd6cb2259e24000371a0e76ddad815b
octagod <ujumaduowen@gmail.com>
2023-12-25 04:47:11 -0500

    minor change: Nicklaus Link Fix

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -83,7 +83,7 @@ const Home = () => {
                     </Flexbox>
                     <Spacebox padding="10px" />
                     <Typography className="by-nick" sx={{fontSize: '10px', letterSpacing: '3px', textTransform: 'uppercase', textAlign: 'center'}}>
-                        This app was built by <a style={{color: "var(--primary)", fontWeight: '700'}} href="https://x.com/owen_nicklauss">Nicklaus</a> with ❤️
+                        This app was built by <a style={{color: "var(--primary)", fontWeight: '700'}} href="https://x.com/owen_nicklaus">Nicklaus</a> with ❤️
                     </Typography>
                     <div className="anim">
                         <img src="/assets/banner_quotely.png" alt="" />
